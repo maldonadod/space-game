b52be7e53300aa160aee7f4d3226ecc940fdb5be
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-26 12:32:02 -0300

    change img repository bg URL

--- src/ImagesRepository.js
+++ src/ImagesRepository.js
@@ -1,7 +1,7 @@
 var ImagesRepository = (function() {
 
   var background = new Image();
-  background.src = "/Users/danielmaldonado/www/canvas-game/src/assets/img/bg.png";
+  background.src = "/assets/img/bg.png";
   return {
     background: background
   }
