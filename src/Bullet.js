3b18bb86dd986a3aef1ea53c5fc224436c8a1d1d
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-27 16:47:29 -0300

    add Bullet,BulletPool,ObjectPool

---
+++ src/Bullet.js
@@ -0,0 +1,37 @@
+var Bullet = (function() {
+
+  function Bullet() {
+    this.x = 0;
+    this.y = 0;
+
+    const { width, height } = ImagesRepository.bullet;
+
+    this.width = width;
+    this.height = height;
+  }
+  Bullet.prototype.context = document.getElementById('main').getContext('2d');
+
+  Bullet.prototype.clear = function() {
+		this.x = 0;
+		this.y = 0;
+		this.speed = 0;
+	};
+
+  Bullet.prototype.draw = function() {
+    this.context.clearRect(this.x, this.y, this.width, this.height);
+		this.y -= this.speed;
+		if (this.y <= 0 - this.height) {
+			return true;
+		}
+		else {
+			this.context.drawImage(ImagesRepository.bullet, this.x, this.y);
+		}
+  }
+  Bullet.prototype.spawn = function(x, y, speed) {
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+	};
+
+  return Bullet;
+})();
